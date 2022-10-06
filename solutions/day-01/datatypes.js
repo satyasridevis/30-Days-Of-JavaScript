f0ed6fc7a117edb7909ec31275ccf875b3ca5b25
Satya Sridevi <66215236+satyasridevis@users.noreply.github.com>
2022-10-06 10:34:37 +0530

    completed day-1 exercises

---
+++ solutions/day-01/datatypes.js
@@ -0,0 +1,15 @@
+let myName = "satya";
+let decision = true;
+let a = undefined;
+let b = null;
+
+console.log(typeof decision);
+console.log(typeof myName);
+console.log(typeof a);
+console.log(typeof b);
+
+let myFirstName = "Satya",
+  myLastName = "Sridevi",
+  maritalStatus = "married(separated)",
+  country = "India",
+  myAge = 32;
---
+++ solutions/day-01/variable.js
@@ -0,0 +1,15 @@
+let name = "satya";
+let decision = true;
+let a = undefined;
+let b = null;
+
+let myName;
+let age;
+let children;
+let hobby;
+
+let myFirstName = "Satya";
+let myLastName = "Sridevi";
+let maritalStatus = "married(separated)";
+let country = "India";
+let myAge = 32;
